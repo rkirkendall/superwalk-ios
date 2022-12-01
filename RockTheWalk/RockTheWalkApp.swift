f883af592a743437ad402b64a1652d5dca1b83fa
Ricky Kirkendall <rkirkendall304@gmail.com>
2022-12-01 13:39:25 -0800

    Initial Commit

---
+++ RockTheWalk/RockTheWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockTheWalkApp.swift
+//  RockTheWalk
+//
+//  Created by Ricky Kirkendall on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RockTheWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
